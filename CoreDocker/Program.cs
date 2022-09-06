e09be7ca11d0fc53310bc2422e34303f3cbaf0bd
Fatih <ffatihsafak@gmail.com>
2022-09-06 23:43:28 +0300

    Add project files.

---
+++ CoreDocker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CoreDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 1000)
+            {
+                Console.WriteLine("Hello World!");
+                i++;
+                System.Threading.Thread.Sleep(5000);
+            }
+
+        }
+    }
+}
